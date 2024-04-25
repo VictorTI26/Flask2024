2fb5de5c3b69ae6a2ca8b9e751e52ebd0f757e4f
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-04-25 12:29:21 -0300

    q

--- app/views.py
+++ app/views.py
@@ -1,13 +1,12 @@
-from app import app,db
+from app import app, db
 from flask import render_template, url_for, request, redirect
 from app.forms import ContatoForm
 from app.models import Contato
 
-
 @app.route('/')
 def homepage():
-    usuario = 'Cabecinha'
-    idade = 34
+    usuario = 'Mateus'
+    idade = 17
 
     context = {
         'usuario':usuario,
@@ -15,18 +14,26 @@ def homepage():
     }
     return render_template('index.html', context=context)
 
-@app.route('/contato/', methods =['GET', 'POST'])
+@app.route('/contato/', methods=['GET', 'POST'])
 def contato():
     form = ContatoForm()
     context = {}
     if form.validate_on_submit():
         form.save()
         return redirect(url_for('homepage'))
-    return render_template ('contato.html', context=context, form=form)
+    return render_template('contato.html', context=context, form=form)
 
 @app.route('/contato/lista/')
 def contatoLista():
-    dados = Contato.query.all()
-    print(dados)
-    context = {'dados':dados}
-    return render_template('contatoLista.html', context=context)
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa', '')
+    dados  = Contato.query.order_by('nome')
+    if pesquisa != '':
+        dados = dados.filter_by(nome=pesquisa)
+    context = {'dados': dados.all()}
+    return render_template('contato_lista.html', context=context)
+
+@app.route('/contato/<int:id>')
+def contatoDetail(id):
+    obj=Contato.query.get(id)
+    return render_template('contato_detail.html', obj=obj)
