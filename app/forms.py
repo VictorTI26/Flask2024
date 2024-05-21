e38bfdb0aebbfedb6a3238deebe40f8f9897d6d3
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-05-21 07:55:33 -0300

    a

--- app/forms.py
+++ app/forms.py
@@ -5,18 +5,19 @@ from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app import db, bcrypt
 from app.models import Contato, User
 
-class Useform(FlaskForm):
+
+class UserForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
     sobrenome = StringField('Sobrenome', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(),Email()])
+    email = StringField('E-mail', validators=[DataRequired(),Email()]) 
     senha = PasswordField('Senha', validators=[DataRequired()])
-    confirmacao_senha = PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confimar senha', validators=[DataRequired(), EqualTo('senha')])
     btnSubmit = SubmitField('Cadastrar')
 
     def validade_email(self, email):
         if User.query.filter(email=email.data).first():
-            return ValidationError('Usuario ja cadastrado com esse E-mail')
-        
+            return ValidationError('Usuário já cadastradado com esse E-mail!!!')
+
     def save(self):
         senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
         user = User(
@@ -29,12 +30,11 @@ class Useform(FlaskForm):
         db.session.commit()
         return user
 
-
 class ContatoForm(FlaskForm):
-    nome = StringField('Nome', validators = [DataRequired()])
-    email = StringField('E-Mail', validators = [DataRequired(), Email()])  # Use Email() here
-    assunto = StringField('Assunto', validators = [DataRequired()])
-    mensagem = StringField('Mensagem', validators = [DataRequired()])
+    nome = StringField('Nome', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired(),Email()])
+    assunto = StringField('Assunto', validators=[DataRequired()])
+    mensagem = StringField('Mensagem', validators=[DataRequired()])
     btnSubmit = SubmitField('Enviar')
 
     def save(self):
@@ -46,3 +46,19 @@ class ContatoForm(FlaskForm):
         )
         db.session.add(contato)
         db.session.commit()
+
+class LoginForm(FlaskForm):
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    btnSubmit = SubmitField('Login')
+
+    def login(self):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.senha, 
+                                        self.senha.data.encode('utf-8')):
+                    return user
+            else:
+                    raise Exception('Senha Incorreta!!!')
+        else:
+            raise Exception('Usuario nao encontrado')
--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from sqlalchemy import Nullable
 from app import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
@@ -7,14 +6,6 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(user_id)
 
-class Contato (db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data_envio = db.Column(db.DateTime, default=datetime.now())
-    nome = db.Column(db.String, nullable=True)    
-    email = db.Column(db.String, nullable=True)
-    assunto = db.Column(db.String, nullable=True)
-    mensagem = db.Column(db.String, nullable=True)
-    respondido = db.Column(db.Integer, default=0)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,3 +13,19 @@ class User(db.Model, UserMixin):
     sobrenome = db.Column(db.String, nullable=True)
     email = db.Column(db.String, nullable=True)
     senha = db.Column(db.String, nullable=True)
+    posts = db.relationship('Post', backref='user', lazy=True)
+
+class Contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data_envio = db.Column(db.DateTime, default=datetime.now())
+    nome = db.Column(db.String, nullable=True)
+    email = db.Column(db.String, nullable=True)
+    assunto = db.Column(db.String, nullable=True)
+    mensagem = db.Column(db.String, nullable=True)
+    respondido = db.Column(db.Integer, default=0)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data_criacao = db.Column(db.DateTime, default=datetime.now())
+    mensagem = db.Column(db.String, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
